cbe0eeac5aa6e669464a5f5dd686dd836a97e862
astiannta14 <153782825+astiannta14@users.noreply.github.com>
2023-12-23 06:37:03 +0700

    Upload File Tugas9

---
+++ 231031083-Asti Ananta-tugas-9-c.py
@@ -0,0 +1,18 @@
+biodata = {"nama"   : "Asti Ananta",
+           "nim"    : 231031083,
+           "prodi"  : "Sistem Informasi C",
+           "umur"   : 18,
+           "hobi"   : "Memasak",
+           "asal"   : "Parepare",
+           "alamat" : "Jalan Jendral Sudirman",
+           "status" : "Mahasiswa",
+           "anak ke": "anak ke 2 dari 2 bersaudara",
+            }
+
+
+print(f"""
+Nama Saya  : {biodata['nama']}
+Dengan NIM : {biodata['nim']}
+Dari Prodi : {biodata['prodi']}
+Hobi Saya  : {biodata['hobi']}
+""")
